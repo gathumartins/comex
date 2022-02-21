265710ef972a480324a4574cbed9c8add8c2f50a
gathumartins <gathumartins@gmail.com>
2022-02-21 19:18:06 +0300

    Header negative margin fixex on small devices

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 function Banner() {
     return (
-        <header className="md:h-[350px] relative -mt-40">
+        <header className="md:h-[350px] relative lg:-mt-40">
             <img src="./imgs/banner.jpeg" className="object-cover bottom-0" alt="Comex homes inner Banner" />
             <section className="bg-comex-primary/60 text-white p-2 absolute w-full bottom-9">
                 <Container>
