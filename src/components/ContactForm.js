f2c9fbc82863da7de094e6ddbfa45741861e3d1b
gathumartins <gathumartins@gmail.com>
2022-03-06 18:36:16 +0300

    added validation to contacts us form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,26 +22,26 @@ function ContactForm() {
           <form ref={form} onSubmit={sendContact} className="mt-10 md:max-w-[1200px] mx-auto">
               <div className="md:flex justify-between gap-10">
                   <div className="w-full relative">
-                      <input type="text" name="firstName" id="firstName" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="FIRST NAME" />
+                      <input type="text" name="firstName" id="firstName" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="FIRST NAME" required/>
                       <label htmlFor="firstName" className="absolute left-0 -top-3 transition-all text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-white peer-focus:text-sm">FIRST NAME</label>
                   </div>
                   <div className="w-full relative">
-                  <input type="text" name="lastName" id="lastName" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="LAST NAME" />
+                  <input type="text" name="lastName" id="lastName" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="LAST NAME" required/>
                       <label htmlFor="lastName" className="absolute left-0 -top-3 transition-all text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-white peer-focus:text-sm">LAST NAME</label>
                   </div>
               </div>
               <div className="md:flex justify-between gap-10">
                   <div className="w-full relative">
-                      <input type="tel" name="phone" id="phone" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="PHONE NUMBER" />
+                      <input type="tel" name="phone" id="phone" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="PHONE NUMBER" required/>
                       <label htmlFor="phone" className="absolute left-0 -top-3 transition-all text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-white peer-focus:text-sm">PHONE NUMBER</label>
                   </div>
                   <div className="w-full relative">
-                      <input type="email" name="email" id="email" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="EMAIL" />
+                      <input type="email" name="email" id="email" className="peer mb-10 bg-transparent h-10 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="EMAIL" required/>
                       <label htmlFor="email" className="absolute left-0 -top-3 transition-all text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-white peer-focus:text-sm">EMAIL</label>
                   </div>
               </div>
               <div className="relative">
-                  <input type="text" name="message" id="message" className="peer mb-10  bg-transparent h-12 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="MESSAGE" />
+                  <input type="text" name="message" id="message" className="peer mb-10  bg-transparent h-12 w-full border-b-2 border-black focus:outline-none focus:border-white placeholder-transparent" placeholder="MESSAGE" required/>
                   <label htmlFor="message" className="absolute left-0 -top-3 transition-all text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-white peer-focus:text-sm">MESSAGE</label>
               </div>
               <div className="text-center my-3">
