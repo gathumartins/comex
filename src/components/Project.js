36e0a8ddd3b264f14193d50802b6b8e07308dc66
gathumartins <gathumartins@gmail.com>
2022-03-09 19:03:15 +0300

    adjusted home page

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,10 +7,10 @@ function Project() {
         <article className="projects relative mt-0 py-[55px]">
             <Container fluid={"lg"} className="min-h-[680px]">
                 <div className="projectsInn md:flex justify-between gap-5">
-                    <div className="d-flex align-items-center md:max-w-{35%]">
+                    <div className="d-flex align-items-center md:max-w-[35%]">
                         <h1 className="text-white text-4xl md:text-5xl lg:text-4xl xl:text-6xl leading-24 font-spartan font-weight-500 text-uppercase inline-block align-middle mb-3">Our<br/> Projects</h1>
                     </div>
-                    <Property className="flex-grow rounded-md" />
+                    <Property className="flex-grow" />
                 </div>
             </Container>
         </article>
